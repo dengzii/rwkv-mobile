e5cad48f1a143487c4e74563db17731dc20c7ba2
Molly Sophia <mollysophia379@gmail.com>
2025-01-15 13:41:14 +0800

    Add simple rwkv-othello test
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- examples/test_othello.cpp
+++ examples/test_othello.cpp
@@ -3,14 +3,14 @@
 
 #include "commondef.h"
 #include "runtime.h"
+#include "c_api.h"
 
 #define ENSURE_SUCCESS_OR_LOG_EXIT(x, msg) if (x != rwkvmobile::RWKV_SUCCESS) { std::cout << msg << std::endl; return 1; }
 
 std::string current = "";
-void callback(const char *msg) {
+void callback(const char *msg, const int) {
     std::cout << msg + current.size();
     current = msg;
-    // std::cout << msg << "\n\n";
 }
 
 int main(int argc, char **argv) {
@@ -21,12 +21,11 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    rwkvmobile::runtime runtime;
-    ENSURE_SUCCESS_OR_LOG_EXIT(runtime.init(argv[3]), "Failed to initialize runtime");
-    ENSURE_SUCCESS_OR_LOG_EXIT(runtime.load_tokenizer(argv[1]), "Failed to load tokenizer");
-    ENSURE_SUCCESS_OR_LOG_EXIT(runtime.load_model(argv[2]), "Failed to load model");
-    runtime.set_sampler_params(1.0, 1, 1.0);
-    runtime.set_penalty_params(0, 0, 0);
+    rwkvmobile_runtime_t runtime = rwkvmobile_runtime_init_with_name(argv[3]);
+    rwkvmobile_runtime_load_tokenizer(runtime, argv[1]);
+    rwkvmobile_runtime_load_model(runtime, argv[2]);
+    rwkvmobile_runtime_set_penalty_params(runtime, {0, 0, 0});
+    rwkvmobile_runtime_set_sampler_params(runtime, {1.0, 1, 1.0});
 
     std::string prompt = "<input>\n"
                         "● ● ● ● ● ● ● ● \n"
@@ -42,13 +41,9 @@ int main(int argc, char **argv) {
                         "MAX_DEPTH-2\n"
                         "</input>\n\n";
 
-    auto ids = runtime.tokenizer_encode(prompt);
-    for (auto id : ids) {
-        std::cout << id << ", ";
-    }
     std::cout << std::endl;
-    std::string result;
+    char result[65536];
 
-    runtime.gen_completion(prompt, result, 64000, 0, callback);
+    rwkvmobile_runtime_gen_completion(runtime, prompt.c_str(), result, 64000, 0, callback);
     return 0;
 }
